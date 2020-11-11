e5e5c6873c7bd5edbd167f696ba81c477124390f
1412qyj <qinyj1309163979@163.com>
2020-11-11 15:41:00 +0800

    master: modify unitest

--- arithmetic/Test6/test6.cpp
+++ arithmetic/Test6/test6.cpp
@@ -14,10 +14,10 @@ namespace Test6
 		{
 			// TODO:  在此输入测试代码
 			LPSTR p_input = new char[MAX_PATH];
-			LPSTR p_output = new char[5];
+			LPSTR p_output = new char[6];
 
 			GetPrivateProfileStringA("test6", "Input", "", p_input, MAX_PATH, INI_PATH);
-			GetPrivateProfileStringA("test6", "Output", "", p_output, 5, INI_PATH);
+			GetPrivateProfileStringA("test6", "Output", "", p_output, 6, INI_PATH);
 
 			stringstream sstr;
 			int inums[MAX_PATH];
@@ -44,6 +44,31 @@ namespace Test6
 
 			Assert::AreEqual(boutput, ContainsDuplicate(ivector_input));
 
+			//===================================================================
+			GetPrivateProfileStringA("test6", "Input1", "", p_input, MAX_PATH, INI_PATH);
+			GetPrivateProfileStringA("test6", "Output1", "", p_output, 6, INI_PATH);
+
+			stringstream sstr1(p_input);
+
+			memset(inums, 0, sizeof(int) * MAX_PATH);
+			i = 0;
+			size = 0;
+			while (sstr1 >> tmp)
+			{
+				inums[i++] = atoi(tmp);
+				size++;
+			}
+
+			ivector_input.clear();
+
+			for (i = 0; i < size; i++)
+			{
+				ivector_input.push_back(inums[i]);
+			}
+
+			boutput = (strncmp("true", p_output, 4) == 0) ? true : false;
+
+			Assert::AreEqual(boutput, ContainsDuplicate(ivector_input));
 		}
 
 	};
--- arithmetic/arithmetic/src/7MaxDepth.cpp
+++ arithmetic/arithmetic/src/7MaxDepth.cpp
@@ -5,10 +5,6 @@ int MaxDepth(TreeNode* root)
 	int maxl = 0;
 	int maxr = 0;
 
-	if (root == nullptr)
-	{
-		return 0;
-	}
 
 	if (root->left != nullptr)
 	{
@@ -27,23 +23,18 @@ TreeNode * CreateTree(int *data, int index, int size)
 {
 	TreeNode *pnode = nullptr;
 
-	if (index < size)
+	if (index > size || data[index] == -1)
+		return nullptr;
+
+	else
 	{
 		pnode = (TreeNode *)malloc(sizeof(TreeNode));
-		if (nullptr == pnode)
-		{
-			perror("malloc");
-			return NULL;
-		}
 
 		pnode->val = data[index];
 		pnode->left = CreateTree(data, 2 * index + 1, size);
 		pnode->right = CreateTree(data, 2 * index + 2, size);
-
-		return pnode;
 	}
 
-	
-	return nullptr;
+	return pnode;
 }
 
